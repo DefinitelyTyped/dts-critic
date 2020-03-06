82371832780c9284ae6f2d671e51563bcf42e353
Gabriela Araujo Britto <t-gaar@microsoft.com>
2020-03-05 16:43:23 -0800

    Update

--- index.ts
+++ index.ts
@@ -641,11 +641,6 @@ function getDtsExportKind(sourceFile: ts.SourceFile): InferenceResult<DtsExportK
         return inferenceSuccess(DtsExportKind.ExportEquals);
     }
     return inferenceSuccess(DtsExportKind.ES6Like);
-    // if (matches(sourceFile, isExportConstruct)) {
-    //     return inferenceSuccess(DtsExportKind.ES6Like);
-    // }
-    // // try get declare module, return eslike if so
-    // return inferenceError("Could not infer export kind of declaration file.");
 }
 
 const exportEqualsSymbolName = "export=";
